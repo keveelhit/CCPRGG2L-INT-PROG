35ffe659cd3ffc691f6557990ba97d9522426ee7
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-01-04 19:39:11 +0800

    Create Number5.java

---
+++ Laboratory Activity 1/Items Source Code/Number5.java
@@ -0,0 +1,38 @@
+package Laboratory1.Number5;
+
+import java.io.*;
+import java.util.*;
+
+public class Number5 {
+  public static void main(String[] args) throws IOException, FileNotFoundException {
+    
+      try (
+      Scanner userInput = new Scanner(System.in)) {
+      System.out.print("File Name: ");
+      String inputFileName = userInput.nextLine();
+      System.out.print("File Name: ");
+      String outputFileName = userInput.nextLine();
+
+
+      File inputFile = new File(inputFileName);
+      PrintWriter output;
+          try (Scanner input = new Scanner(inputFile)) {
+              FileWriter outputFile = new FileWriter(outputFileName);
+              output = new PrintWriter(outputFile);
+              while (input.hasNextLine()) {
+                  String line = input.nextLine();
+                  String[] words = line.split(" ");
+                  for (int i = words.length - 1; i >= 0; i--) {
+                      output.print(words[i] + " ");
+                  }
+                  
+                  output.println();
+                  
+              }   
+          }
+          
+      output.close();
+      
+    }
+  }
+}
