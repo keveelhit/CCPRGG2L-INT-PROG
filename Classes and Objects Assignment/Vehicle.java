cbb19e721afb0c93cc2134c9c382e0bf58af222e
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-01-30 19:25:38 +0800

    Create Vehicle.java

---
+++ Classes and Objects Assignment/Vehicle.java
@@ -0,0 +1,25 @@
+public class Vehicle{
+
+    // Variables of Attributes
+    String brand;
+    String model;
+    int EngineDisplacement;
+    int FuelCapacity;
+  
+    // Methods and Method Names
+    public void sayMyBrand(){
+      System.out.println("Brand Name: " + brand);
+    }
+  
+    public void sayMyModel(){
+      System.out.println("Model: " + model);
+    }
+  
+    public void sayMyEngineDisplacement(){
+      System.out.println("Engine Displacement (cc): " + EngineDisplacement);
+    }
+  
+    public void sayMyFuelCapacity(){
+      System.out.println("Fuel Capacity (L): " + FuelCapacity);
+    }
+  }
