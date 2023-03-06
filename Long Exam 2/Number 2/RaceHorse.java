2a2adf154bb641653b7009b188c1a3d6ca3c34cc
Karen Pearl Pabilando <55318698+keveelhit@users.noreply.github.com>
2023-03-06 12:07:17 +0800

    Create RaceHorse.java

---
+++ Long Exam 2/Number 2/RaceHorse.java
@@ -0,0 +1,12 @@
+
+public class RaceHorse extends Horse{
+    private int numOfRaces;
+   
+    public int getNumOfraces() {
+        return numOfRaces;
+    }
+    
+    public void setNumOfRaces(int numOfRaces) {
+        this.numOfRaces = numOfRaces;
+    } 
+}
